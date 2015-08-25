961d735cc4aa0c92e930f6b92aa94afee7deec38
kabletown-ux <richard_ruiz@cable.comcast.com>
2015-08-24 23:08:44 -0400

    Added speaker field(s) renaming

--- round2.R
+++ round2.R
@@ -1,12 +1,31 @@
 loadData <- function( fileName ) {
     
-    rawData <<- read.csv( paste( "/Users/rruiz/Work/biometrics/python-src/data/2015.06.16-output/", fileName, sep = '' ) )
+    rawData <- read.csv( paste( "data/", fileName, sep = '' ) )
+    #print( str( rawData ) )
     
-    ## update all speaker.test.score fields to "NA" if their comment field != "trained"
-    #o <<- rawData$real.speaker[ rawData$real.speaker == "o", ]
+    # coerce all factors to character: http://stackoverflow.com/questions/2851015/convert-data-frame-columns-from-factors-to-characters/2853231#2853231
+    i <- sapply( rawData, is.factor )
+    rawData[ i ] <- lapply( rawData[ i ], as.character )
+    
+    #print( str( rawData ) )
+    
+    # update levels first, a *must* for factors
+    levels( rawData$real.speaker ) <- c( "o gabrielle borneman", "errinn rebollar", "novela soto", "russel isabell", "robert ryan" )
+    
+    #print( levels( rawData$real.speaker ) )
+    
+    # update field names: real.speaker
     rawData$real.speaker[ rawData$real.speaker == "o" ] <- "o gabrielle borneman"
-    #rawData$imposter.test.score[ rawData$training.comment != "trained" ] <- NA
+    rawData$real.speaker[ rawData$real.speaker == "errinn" ] <- "errinn rebollar"
+    rawData$real.speaker[ rawData$real.speaker == "novella" ] <- "novella soto"
+    rawData$real.speaker[ rawData$real.speaker == "russel" ] <- "russel isabell"
+    rawData$real.speaker[ rawData$real.speaker == "robert" ] <- "robert ryan"
+    # update field names: predicted.speaker
+    rawData$predicted.speaker[ rawData$predicted.speaker == "o" ] <- "o gabrielle borneman"
+    rawData$predicted.speaker[ rawData$predicted.speaker == "errinn" ] <- "errinn rebollar"
+    rawData$predicted.speaker[ rawData$predicted.speaker == "novella" ] <- "novella soto"
+    rawData$predicted.speaker[ rawData$predicted.speaker == "russel" ] <- "russel isabell"
+    rawData$predicted.speaker[ rawData$predicted.speaker == "robert" ] <- "robert ryan"
     
-    ## remove rows w/ NA.  
-    #rawData <- rawData[ complete.cases( rawData ), ]
+    #rawData <<- rawData
 }
